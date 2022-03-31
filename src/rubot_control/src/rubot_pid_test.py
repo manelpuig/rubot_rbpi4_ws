341a90058d75356c77aacd809aecc0cd05f7ae4a
Dr. Manel Puig i Vidal <36498743+manelpuig@users.noreply.github.com>
2022-03-31 16:58:14 +0200

    pid test write

--- src/rubot_control/src/rubot_pid_test.py
+++ src/rubot_control/src/rubot_pid_test.py
@@ -3,14 +3,41 @@ import sys
 import rospy
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
+import matplotlib.pyplot as plt
+import numpy as np
+from enum import Enum
+import csv
 
 robot_x = 0.0
+sample = 0
+tplot=[]
+xplot = []
+vxplot=[]
+fig, ax = plt.subplots()
+plt.ion()
+#maxlen = 400
 
 def odom_callback(data):
     global robot_x
+    global sample
+    global tplot
+    global xplot
+    global vxplot
+    sample +=1
+    tplot.append(sample)
     robot_x=data.pose.pose.position.x
     rospy.loginfo("Robot Odometry x= %f\n",robot_x)
-    
+    xplot.append(robot_x)
+    robot_vx=data.twist.twist.linear.x
+    rospy.loginfo("Robot Odometry vx= %f\n",robot_x)
+    vxplot.append(robot_vx)
+    plt.plot(tplot, vxplot, color='r')
+    plt.plot(tplot, xplot, color='b')
+    plt.pause(0.05)
+    ax.clear()
+    fig.ylim([0, 120])
+    fig.legend(["Vx and X"],loc = "upper left")
+    plt.show()       
     
 	
 def move_rubot(lin_vel,ang_vel,distance):
@@ -38,6 +65,14 @@ def move_rubot(lin_vel,ang_vel,distance):
             vel.angular.y = 0.0
             vel.angular.z = 0.0
             pub.publish(vel)
+
+            with open('CL_PIDcontrol.csv', 'w') as csvfile:
+            header = ['vx','xh']
+            writer = csv.DictWriter(csvfile,fieldnames=header)
+            writer.writeheader()
+            for i in range(0,len(vxplot):
+                writer.writerow({'vx': vxplot[i],
+                                'x': xplot[i]})
             break 
         else:
             rospy.loginfo("Robot moving")
